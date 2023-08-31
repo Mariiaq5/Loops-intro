aa52f294ae267fded3a320b8ac6bad3120b893da
Mary Williams <mary.williams@gmail.com>
2023-08-31 11:18:28 -0400

    your commit message

---
+++ main.js
@@ -0,0 +1,14 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+for(var i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i]);
+}
+
+var myInterest = ["Traveling", "Coding", "Music", "Shopping", "Sport"]
+for (var i = 0; i < myInterest.length; i++){
+    console.log(`One of my interest is: ${myInterest[i]}`);
+    console.log(`My absolute favorite interest is ${myInterest[0]}`);
+    console.log(`One of my interest is ${myInterest[1]}`);
+    console.log(`One of my interest is ${myInterest[2]}`);
+    console.log(`One of my interest is ${myInterest[3]}`);
+    console.log(`One of my interest is ${myInterest[4]}`)
+}
